e0d827141fc4a0579d9fcdb306dbe14665e0f3ee
毛亚琛 <maoyachen55@gmail.com>
2023-05-08 14:33:57 +0800

    fix cluster

--- data.py
+++ data.py
@@ -10,12 +10,17 @@ def load_dataset(offset=0, limit=100, group=None):
     else:
         filter = {}
     global CACHE
-    if limit == -1 and group is None:
-        if CACHE is None:
+    if limit == -1:
+        if group is None:
+            if CACHE is None:
+                records = db["dataset"].find(filter)
+                df = pd.DataFrame(list(records))
+                CACHE = df
+            return CACHE
+        else:
             records = db["dataset"].find(filter)
             df = pd.DataFrame(list(records))
-            CACHE = df
-        return CACHE
+            return df
     else:
         records = db["dataset"].find(filter=filter, skip=offset, limit=limit)
         df = pd.DataFrame(list(records))
